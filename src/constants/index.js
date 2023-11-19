62630e5d983cb753f60d9e305646284c4fbffda6
Adnaan Hussain <adnaan137@gmail.com>
2023-11-19 18:48:44 +0530

    Fair not Final

---
+++ src/constants/index.js
@@ -0,0 +1,230 @@
+import { meta, shopify, tesla, starbucks } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Process Associate",
+    company_name: "Marble Box",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "September 2019 - December 2020",
+    points: [
+      "Helped meet changing demands by recommending improvements to business systems or procedures.",
+      "Analyzed process flows and identifies areas of improvement to increase productivity.",
+      "Developed standard operating procedures workflows to drive current and future processes.",
+      "Reported on process performance to verify compliance with established standards on process performance to verify compliance with established standards.",
+      "Partnered with cross-functional teams to conduct thorough discovery and due diligence on existing processes.",
+    ],
+  },
+  {
+    title: "Business Development Intern",
+    company_name: "ShapeAI",
+    icon: tesla,
+    iconBg: "#fbc3bc",
+    date: "Mar 2021 - Oct 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Freelance",
+    icon: shopify,
+    iconBg: "#b7e4c7",
+    date: "Oct 2021 - Oct 2022",
+    points: [
+      "Developing and maintaining web applications using HTML,CSS, and Javascript and other related technologies.",
+      "Created a Website for a Local Cafe in my Hometown.",
+      "Tried Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in Various Masterclasses by Scaler or Devtown or 10x Academy.",
+    ],
+  },
+  {
+    title: "Front-end Developer",
+    company_name: "",
+    icon: meta,
+    iconBg: "#a2d2ff",
+    date: "Nov 2022 - Present",
+    points: [
+      "Led Collaborative Efforts: Spearheaded collaboration with the marketing team, serving as the liaison for the web team to define project goals, projections, and milestones.",
+      "Strategically Planned Web Development: Devised a strategic plan to skillfully transform mockups into a functional websites using HTML, JavaScript/React, Prisma, Clerk.",
+      "Executed SEO and Ad Campaigns: Implemented and regulated Google-based SEO and Ad campaigns, ensuring alignment with budget specifications.",
+      "Developed Websites: Proficiently coded websites using HTML, CSS, React, and NextJS, achieving a seamless user experience.",
+      "Designed and Enhanced Web Components: Designed, implemented, and proactively monitored web pages, plugins, and functionality to drive continuous improvement",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/YourGitHubUsername",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Youtoob",
+    description:
+    "Built a Youtube Clone to understand the structure of a Huge User Base Applications. This is 100% Mobile Responsive and without Ads.",
+    link: "https://github.com/Adnaanlegend/Youtoob",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Nemesis AI",
+    description:
+      "Using Next.JS, Tailwind, Prisma, Stripe, Open Ai and Crisp for Customer Support, I built a SAAS Platform, Where users get Conversation AI and many more features. *Under Maintenance mode* ",
+    link: "https://github.com/Adnaanlegend/Saas-AI",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Basket Dashboard",
+    description:
+      "Built with React for dynamic interactivity, TailwindCSS  sleek styling, and enhanced with Apex Charts and Framer Motion for data visualization and animations respectively. 100% Mobile responsive.",
+    link: "https://github.com/Adnaanlegend/Basket",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Gallery App AI",
+    description:
+      "It was my first Next.JS Project and I tried to make it with cloudinary and Shadcn-ui, There are a lot of features.Give it a go.",
+    link: "https://github.com/Adnaanlegend/galleryapp",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Lyriks",
+    description:
+      "Developed a music player using React, Tailwind, Redux, RapidApi. 100% Mobile Responsive",
+    link: "https://mega.nz/file/1zBB0BhI#wK8n3rYKwqXTL5QDTTUyAuA6cvOMOngCjxdb-Uzsc14",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "Threads Clone",
+    description:
+      "Currently working on it.",
+    link: "",
+  },
+];
